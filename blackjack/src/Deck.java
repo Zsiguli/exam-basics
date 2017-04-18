58da631e34317fbc9e444416ef4e7360a3f19862
zsiguli <markovichzsigmond@gmail.com>
2017-04-18 10:39:19 +0200

    feat(deck): add the same card to the deck

--- blackjack/src/Deck.java
+++ blackjack/src/Deck.java
@@ -1,9 +1,15 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Deck {
   final static String[] colors = {"Clubs", "Diamonds", "Hearts", "Spades"};
   final static String[] values = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
 
-  private List<Card> cardList;
+  private List<Card> cardList = new ArrayList<>();
 
-}
+  public Deck(int numberOfCardsInTheDeck) {
+    for (int i = 0; i < numberOfCardsInTheDeck; ++i) {
+      cardList.add(new Card(colors[0], values[0]));
+    }
+  }
+ }
