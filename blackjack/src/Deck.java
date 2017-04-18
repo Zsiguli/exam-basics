f755051ecf867d5ecff5fcb999ea0bd8d5d49b9a
zsiguli <markovichzsigmond@gmail.com>
2017-04-18 10:32:18 +0200

    feat(deck): implement possible card types

--- blackjack/src/Deck.java
+++ blackjack/src/Deck.java
@@ -1,7 +1,9 @@
 import java.util.List;
 
 public class Deck {
-  
+  final static String[] colors = {"Clubs", "Diamonds", "Hearts", "Spades"};
+  final static String[] values = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+
   private List<Card> cardList;
 
 }
