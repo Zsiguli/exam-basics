220d43af2beae3e25fc8c8abcbb0ad15084160c4
zsiguli <markovichzsigmond@gmail.com>
2017-04-18 10:08:45 +0200

    feat(oddAvg): pass two test

--- oddavg/src/OddAvg.java
+++ oddavg/src/OddAvg.java
@@ -1,7 +1,28 @@
+import java.util.List;
+
 public class OddAvg {
   public static void main(String[] args) {
     // Create a function called `oddAverage` that takes a list of numbers as parameter
     // and returns the average value of the odd numbers in the list
     // Create basic unit tests for it with at least 3 different test cases
+  }
+
+  public static double oddAverage(List listOfNumbers) {
+    int wholeValue = 0;
+    int oddCounter = 0;
+    double averageValue;
+    for (int i = 0; i < listOfNumbers.size(); ++i) {
+      int actualNumber = (int) listOfNumbers.get(i);
+      if (actualNumber % 2 != 0) {
+        wholeValue += actualNumber;
+        ++oddCounter;
+      }
+    }
+    try {
+      averageValue = wholeValue / oddCounter;
+      return averageValue;
+    } catch (ArithmeticException ex) {
+      return .0;
+    }
   }
 }
